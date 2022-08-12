73d25667c992b5c5e9906d7d9f6d5d740ab85e28
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-13 01:45:09 +0700

    add void loop

--- PotensioWithLed/Potensio_LED.ino
+++ PotensioWithLed/Potensio_LED.ino
@@ -5,3 +5,13 @@ void setup()
 pinMode(A0, INPUT);
 pinMode(9, OUTPUT);
 }
+
+void loop()
+{
+	sensorVal = analogRead(A0);
+  	digitalWrite(9,1);
+  	delay(sensorVal);
+  	digitalWrite(9,0);
+  	delay(sensorVal);
+  
+}
