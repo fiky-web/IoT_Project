6d06618a0ef2c703ad8f20f0eaed6b417ca28c52
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-13 01:41:12 +0700

    add void loop

--- rgb_leds/RGB_LED.ino
+++ rgb_leds/RGB_LED.ino
@@ -5,3 +5,20 @@ void setup(){
   	pinMode(12, OUTPUT);
   	pinMode(13, OUTPUT);
 }
+
+void loop(){
+  for(int i = 0 ; i<150;i+=5){
+      analogWrite(11, i+50);
+      analogWrite(12, i);
+      analogWrite(13, i+100);
+      delay(150);
+
+    for (int j=0 ; j<150; j+=15){
+      analogWrite(11, i+25);
+      analogWrite(12, j+105);
+      analogWrite(13, i);
+      delay(50);
+    }
+
+}
+}
